398da1cbf2a097edc3cceb88f517ae330146a61e
Germán Larraín <glarrain@users.noreply.github.com>
2019-02-20 19:12:43 -0300

    create app `fd_accounts`
    Reusable Django app to replace Django's default auth user account
    model located in `django.contrib.auth`.

---
+++ fd_accounts/__init__.py
@@ -0,0 +1,7 @@
+"""
+Reusable Django app to replace Django's default auth user account model.
+
+The default model is located in :mod:`django.contrib.auth`.
+
+"""
+default_app_config = 'fd_accounts.apps.AccountsAppConfig'
---
+++ fd_accounts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountsAppConfig(AppConfig):
+    name = 'fd_accounts'
+    verbose_name = 'Fyndata Accounts'
