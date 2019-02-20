0accda66911b059ebb756348a3bd0c61a99d7ee1
Germán Larraín <glarrain@users.noreply.github.com>
2019-02-20 19:53:28 -0300

    app: rename package/dir to `fd_dj_accounts`
    (overwrite the existing dir contents)
    
    Nothing of the moved code was changed except that the version
    definition that existed in 'fd_dj_accounts/__init__.py' was
    preserved.

--- fd_dj_accounts/__init__.py
+++ fd_dj_accounts/__init__.py
@@ -1,4 +1,10 @@
+"""
+Reusable Django app to replace Django's default auth user account model.
+
+The default model is located in :mod:`django.contrib.auth`.
+
+"""
 __version__ = '0.0.2'
 
 
-default_app_config = 'fd_dj_accounts.apps.AccountsAppConfig'
+default_app_config = 'fd_accounts.apps.AccountsAppConfig'
--- fd_dj_accounts/apps.py
+++ fd_dj_accounts/apps.py
@@ -1,5 +1,29 @@
 from django.apps import AppConfig
+from django.conf import settings
+import django.core.exceptions
+import django.core.validators
 
 
 class AccountsAppConfig(AppConfig):
-    name = 'fd_dj_accounts'
+
+    name = 'fd_accounts'
+    verbose_name = 'Fyndata Accounts'
+
+    def ready(self) -> None:
+        _validate_app_settings()
+
+
+def _validate_app_settings() -> None:
+    # note: we don't validate that setting 'AUTH_USER_MODEL' is set to 'fd_accounts.User' because
+    #   there might be other reason for a project to use this app without changing that setting.
+
+    try:
+        system_user_email_address = settings.FD_ACCOUNTS_SYSTEM_USER
+    except AttributeError as exc:
+        msg = "Setting 'FD_ACCOUNTS_SYSTEM_USER' must be set."
+        raise django.core.exceptions.ImproperlyConfigured(msg) from exc
+    try:
+        django.core.validators.validate_email(system_user_email_address)
+    except django.core.exceptions.ValidationError as exc:
+        msg = "Setting 'FD_ACCOUNTS_SYSTEM_USER' value is not a valid email address."
+        raise django.core.exceptions.ImproperlyConfigured(msg) from exc
