286d91b6f5abc998eba46984c86c02b1541343cc
Jose Tomas Robles Hahn <jtrobles@cordada.com>
2022-08-19 20:04:09 -0400

    chore(management): Improve tests of management command `createsuperuser`
    Ref: https://cordada.aha.io/features/COMPCLDATA-112

--- tests/test_management.py
+++ tests/test_management.py
@@ -3,19 +3,29 @@ from io import StringIO
 from django.core.management import call_command
 from django.test import TestCase
 
+from fd_dj_accounts.models import User
+
 
 class CreatesuperuserManagementCommandTestCase(TestCase):
-    def test_command_minimal(self) -> None:
+    def test_basic_usage(self) -> None:
+        """
+        Check the operation of the createsuperuser management command
+        """
+        # We can use the management command to create a superuser
         new_io = StringIO()
-
         call_command(
             'createsuperuser',
             interactive=False,
             email_address='staff@example.com',
-            verbosity=0,
-            stdout=new_io,
-            stderr=new_io,
+            stdout=new_io
         )
+        command_output = new_io.getvalue().strip()
+        self.assertEqual(command_output, 'Superuser created successfully.')
+        u = User.objects.get(email_address='staff@example.com')
+        self.assertEqual(u.get_username(), 'staff@example.com')
+
+        # created password should be unusable
+        self.assertFalse(u.has_usable_password())
 
     # TODO: Add tests.
     #   See:
