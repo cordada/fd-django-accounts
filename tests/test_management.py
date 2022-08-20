f3d2a11602fc19669f7a7ed00b68a341c91bed59
Jose Tomas Robles Hahn <jtrobles@cordada.com>
2022-08-19 20:06:42 -0400

    chore(management): Add tests for helper function `get_default_username`
    Ref: https://cordada.aha.io/features/COMPCLDATA-112

--- tests/test_management.py
+++ tests/test_management.py
@@ -3,7 +3,16 @@ from io import StringIO
 from django.core.management import call_command
 from django.test import TestCase
 
+from fd_dj_accounts import management
 from fd_dj_accounts.models import User
+
+
+class GetDefaultUsernameTestCase(TestCase):
+    def test_simple(self) -> None:
+        self.assertEqual(management.get_default_username(), '')
+
+    # TODO: Add tests.
+    #   See: https://github.com/django/django/blob/3.2/tests/auth_tests/test_management.py#L104-L139
 
 
 class CreatesuperuserManagementCommandTestCase(TestCase):
